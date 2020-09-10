914a7a5b546b3ff145464decc3a67411cb2f0b66
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 10:12:15 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
-
-
+import { ProjectComponent } from './project/project.component';
+import { DepartmentComponent } from './department/department.component';
 
 const routes: Routes = [ 
    
     { path: 'employee', component: EmployeeComponent },
     { path: 'employeeList', component: EmployeeListComponent },
+     { path: 'project', component: ProjectComponent},
+      { path: 'department', component: DepartmentComponent },
  ];
 
 @NgModule({
