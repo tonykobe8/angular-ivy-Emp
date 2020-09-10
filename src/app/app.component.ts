6bf15474d6e78ed218ea9e8ad1856d234aab5d92
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 12:30:35 +0200

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
- import {Router } from '@angular/router';
+ import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
 
  import {DialogComponent} from './dialog/dialog.component';
