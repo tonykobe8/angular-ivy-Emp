4fb33b3f159b23906f369d3965941aebf8ee33c5
tonykobe8 <tonykobe8@gmail.com>
2020-09-13 21:53:46 +0200

    3 files modified

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -18,7 +18,7 @@ HR_Employees =
     {FirstName:"Portia", LastName: "Mashaba", Title: "Software Developer", Age:21, img:"https://images.assetsdelivery.com/compings_v2/michaeljung/michaeljung1302/michaeljung130200277.jpg" },
     {FirstName:"Nelly", LastName: "Msiza", Title: "Business Analyst", Age:24, img: "https://us.123rf.com/450wm/francesco83/francesco831502/francesco83150200037/36350352-photo-of-african-smiling-businessman-standing-next-to-the-glass-wall.jpg?ver=6"},
     {FirstName:"Portia", LastName: "Mashaba", Title: "Software Developer", Age:21, img:"https://www.lsmensclothing.com/wp-content/uploads/2018/10/what-color-suit-to-buy.jpeg" }]
-    Accounting_Employees=[
+    Accounting_Employees =[
     {FirstName:"Nelly", LastName: "Msiza", Title: "Business Analyst", Age:24, img: "https://cdn.cbtnews.com/wp-content/uploads/2017/04/shutterstock_403315558.jpg"}, 
     {FirstName:"Portia", LastName: "Mashaba", Title: "Software Developer", Age:21, img:"https://image.shutterstock.com/image-photo/african-american-customer-support-operator-260nw-1698411634.jpg" },
     {FirstName:"Nelly", LastName: "Msiza", Title: "Business Analyst", Age:24, img: "https://secureservercdn.net/192.169.221.188/ca0.384.myftpupload.com/wp-content/uploads/2015/02/5-startegies-for-finding-a-dream-job.jpg"}]; 
