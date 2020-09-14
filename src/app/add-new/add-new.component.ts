3fb423c88f8c79191645ea2e188d51c0f87f7e95
tonykobe8 <tonykobe8@gmail.com>
2020-09-14 13:45:41 +0200

    2 files modified

--- src/app/add-new/add-new.component.ts
+++ src/app/add-new/add-new.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-add-new',
   templateUrl: './add-new.component.html',
